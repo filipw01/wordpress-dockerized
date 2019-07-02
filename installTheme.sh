a63d4727f03d5dfc9942b87d42228e6816eb3049
Filip <filipw01@interia.eu>
2019-07-02 13:11:09 +0200

    Update installTheme.sh

--- installTheme.sh
+++ installTheme.sh
@@ -2,13 +2,15 @@
 echo "Choose theme (lumberjack, default)";
 read theme;
 echo "Using theme: $theme";
-chmod 755 wordpress mysql -R;
 if [ "$theme" = "lumberjack" ] ; then 
   git clone https://github.com/MrkKr/wp_lumberjack_boilerplate ./wordpress/wp-content/themes/lumberjack-theme ;
   cd wordpress/wp-content/themes/lumberjack-theme;
   composer require rareloop/lumberjack-core;
+  git remote remove origin;
 else
   git clone https://github.com/MrkKr/wp_boilerplate ./wordpress/wp-content/themes/default-theme ;
   cd wordpress/wp-content/themes/default-theme;
+  git remote remove origin;
 fi
+chmod 777 wordpress -R;
 echo "Ready setting up your theme";
