0c259ab14c9b585f1364d75aff5b36f888468f30
Filip <filipw01@interia.eu>
2019-07-09 14:06:02 +0200

    Remove .git folder when installing theme with installTheme.sh

--- installTheme.sh
+++ installTheme.sh
@@ -7,10 +7,12 @@ if [ "$theme" = "lumberjack" ] ; then
   cd wordpress/wp-content/themes/lumberjack-theme;
   composer require rareloop/lumberjack-core;
   git remote remove origin;
+  rm .git -R
 else
   git clone https://github.com/MrkKr/wp_boilerplate ./wordpress/wp-content/themes/default-theme ;
   cd wordpress/wp-content/themes/default-theme;
   git remote remove origin;
+  rm .git -R
 fi
 chmod 777 wordpress -R;
 echo "Ready setting up your theme";
