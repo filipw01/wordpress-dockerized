e7aa55772c40563f6d8994d8bbc2e46e65137017
Filip Wachowiak <filipw01@interia.eu>
2020-02-20 21:36:29 +0100

    Add force when removing .git in script

--- installTheme.sh
+++ installTheme.sh
@@ -6,11 +6,11 @@ if [ "$theme" = "lumberjack" ] ; then
   git clone https://github.com/MrkKr/wp_lumberjack_boilerplate ./wordpress/wp-content/themes/lumberjack-theme ;
   cd wordpress/wp-content/themes/lumberjack-theme;
   composer require rareloop/lumberjack-core;
-  rm -R .git
+  rm -Rf .git
 else
   git clone https://github.com/MrkKr/wp_boilerplate ./wordpress/wp-content/themes/default-theme ;
   cd wordpress/wp-content/themes/default-theme;
-  rm -R .git
+  rm -Rf .git
 fi
 chmod -R 777 ../../../../wordpress;
 echo "Ready setting up your theme";
